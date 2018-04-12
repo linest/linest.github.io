6476199b437ec3de5fefb3a9917f9bed6bbc943e
Li Xu <xlinest@gmail.com>
2018-04-13 01:30:25 +0800

    Site updated: 2018-04-13 01:30:19

--- js/reward.js
+++ js/reward.js
@@ -1,9 +1,11 @@
 define([], function(){
 	return {
 		init: function(){
-			document.getElementById('btn_donate').onclick = function() {
-				$('#donate_board').addClass('hidden');
-				$('#donate_guide').removeClass('hidden');
+			if(document.getElementById('btn_donate')){
+				document.getElementById('btn_donate').onclick = function() {
+					$('#donate_board').addClass('hidden');
+					$('#donate_guide').removeClass('hidden');
+				}
 			}
 		}
 	}
