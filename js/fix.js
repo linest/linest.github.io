16cc01743bd64ab07d97fdb782a003800baaf44f
Li Xu <xlinest@gmail.com>
2018-12-18 23:26:39 +0800

    Site updated: 2018-12-18 23:26:27

--- js/fix.js
+++ js/fix.js
@@ -2,14 +2,26 @@ define([], function(){
     return {
 		exec: function(){
             //使文章中的表格具有滚动条功能
-            var article = document.querySelector(".article-entry");
-            var tables = document.querySelectorAll(".article-entry > table");
+            var article = document.querySelector(".article-entry")
+            var tables = document.querySelectorAll(".article-entry > table")
             tables.forEach(function(table){
                 var wrapperDiv = document.createElement("div");
                 wrapperDiv.style.overflow = "auto";
                 article.insertBefore(wrapperDiv,table);
                 wrapperDiv.appendChild(table);
-            });
+            })
+			
+			//禁止右键菜单
+			document.oncontextmenu = function(evt){ 
+				evt.preventDefault() 
+			}
+			
+			//禁止选择
+			document.onselectstart = function(evt){ 
+				console.log(event.target)
+				//'figure.highlight'
+				//evt.preventDefault() 
+			}
 		}
 	}
 });
