7f6a74f75771a167211865090e7aaf451e1df937
Li Xu <xlinest@gmail.com>
2019-12-07 09:12:54 +0800

    Site updated: 2019-12-07 09:12:34

---
+++ 2018/09/16/special-2euro-commemorative-coins/2019.js
@@ -0,0 +1,29 @@
+/*
+    {
+        feature:'',
+        description:'',
+        image:'',
+        volumn:'',
+        date:'',
+        country:'',
+        series: ''
+    }
+*/
+collection[2019] = [
+    {
+        feature:'欧洲货币机构建立25周年',
+        description:'硬币内部右侧描绘了EMI首任主席Alexandre Lamfalussy的肖像，下方刻有名字。左侧的缩写EMI上方刻有年份1994，表示该机构的成立以及Lamfalussy担任其第一任主席的日期。在“EMI”下方，描绘了几枚硬币叠放，从上至下分别刻有€，“ECU”和“BEF”字样。鉴于是比利时发行，选择了“BEF”，即以前的本国货币的缩写。这种展现的目的是象征本国货币向欧洲单一货币的过渡，以欧元为基础建立包括欧洲央行和新货币在内的欧洲中央银行体系。硬币的左上方有铭文“European Monetary Institute”。由于是荷兰皇家造币厂铸造，乌得勒支的造币厂标志墨丘利权杖位于左侧，比利时造币厂长标志是赫泽勒市的徽章。国家代码BE和年份2019位于底部。右面刻有代表硬币设计者Luc Luycx先生的缩写LL。硬币的外圈带有欧盟的12星。',
+        image:'https://www.ecb.europa.eu/euro/coins/comm/shared/img/comm_2019_belgium_25anniv_EMI.jpg',
+        volumn:'15.5万枚',
+        date:'2019.01',
+        country:'比利时'
+    },
+    {
+        feature:'老彼得·勃鲁盖尔逝世450周年',
+        description:'硬币的内部描绘了比利时著名画家老彼得·勃鲁盖尔（Pieter Bruegel the Elder）的肖像以及画架上的绘画。 上方可以找到名称P. BRUEGEL，年份1569，带有一个小的方尖碑符号表示逝世年份，2019是发行年份。 由于是荷兰皇家铸币厂铸造，乌得勒支的造币厂标志墨丘利权杖与比利时造币厂长标志赫泽勒市的徽章和国家代码BE一起位于左侧。 缩写LL代表硬币的设计师Luc Luycx先生，位于右侧。 最后，在内部边缘有一圈小圆点。 硬币的外圈带有欧盟的12星。',
+        image:'https://www.ecb.europa.eu/euro/coins/comm/shared/img/comm_2019_450anniv_PieterBreugeltheelder.jpg',
+        volumn:'15.5万枚',
+        date:'2019.01',
+        country:'比利时'
+    }
+]
